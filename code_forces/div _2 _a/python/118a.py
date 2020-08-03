3841afbda9f30d7c5655d19b079163b5e13bd464
Yogita Verma <veryogita@gmail.com>
2020-08-03 21:01:18 +0530

    Delete 118a.py

--- code_forces/div _2 _a/python/118a.py
+++
@@ -1,9 +0,0 @@
-myString = input()
-vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-result = ''
-for letter in myString:
-    if letter.isupper():
-        letter = letter.lower()
-    if letter not in vowels:
-        result = result + '.' + letter
-print (result)
