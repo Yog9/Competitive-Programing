140db96bd4b4d0e362eb15cf66e5b73950c6b231
Yogita Verma <veryogita@gmail.com>
2020-08-28 17:14:49 +0530

    Create smallest after deleting given elements.py

---
+++ Geeks for Geeks/Interview Preparation/Frequency Counter Pattern/smallest after deleting given elements.py
@@ -0,0 +1,3 @@
+# Find the smallest after deleting given elements
+# https://www.geeksforgeeks.org/find-the-smallest-after-deleting-given-elements/
+# Given an array of integers, find the smallest number after deleting given elements. In case of repeating elements, we delete one instance (from the original array) for every instance present in the array containing elements to be deleted.
