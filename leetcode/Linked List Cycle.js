66e5a0e479ef30cbb69b946095e245896efe0ee2
Yogita Verma <veryogita@gmail.com>
2020-08-31 20:00:26 +0530

    Linked List Cycle Leetcode

---
+++ leetcode/Linked List Cycle.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/linked-list-cycle/
+//  Linked List Cycle
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+var hasCycle = function(head) {
+    let slowPointer = head
+    let fastPointer = head
+    while(fastPointer !== null && fastPointer.next !== null ){
+        slowPointer = slowPointer.next
+        fastPointer = fastPointer.next.next
+        if (slowPointer == fastPointer){
+            return true 
+        } 
+    }
+    return false
+};
