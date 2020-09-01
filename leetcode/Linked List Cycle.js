061bb68be653fc51703577222b35be972a7fb052
Yogita Verma <veryogita@gmail.com>
2020-09-01 22:20:38 +0530

    Update Linked List Cycle.js

--- leetcode/Linked List Cycle.js
+++ leetcode/Linked List Cycle.js
@@ -1,26 +1,69 @@
 // https://leetcode.com/problems/linked-list-cycle/
 //  Linked List Cycle
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
+//  Loop Detectection in Singly linked List
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
 
-var hasCycle = function(head) {
-    let slowPointer = head
-    let fastPointer = head
-    while(fastPointer !== null && fastPointer.next !== null ){
-        slowPointer = slowPointer.next
-        fastPointer = fastPointer.next.next
-        if (slowPointer == fastPointer){
-            return true 
-        } 
+  push(val) {
+    var newNode = new Node(val);
+    var current;
+    if (!this.head) {
+      this.head = newNode;
+      this.length++;
+      return;
+    } else {
+      current = this.head;
     }
-    return false
-};
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+    this.length++;
+  }
+
+  detectLoop() {
+    if (this.head == null || this.head.next == null) {
+      return null;
+    }
+    var slowP = this.head;
+    var fastP = this.head;
+    while (fastP && fastP.next) {
+      slowP = slowP.next;
+      fastP = fastP.next.next;
+      if (fastP === slowP) {
+        console.log("Loop detected is true");
+        return true;
+      }
+    }
+    if (slowP != fastP) {
+      console.log("Loop detected false");
+      return false;
+    }
+  }
+}
+
+function createCyclicLinkedList() {
+  let list = new LinkedList();
+  list.push(0);
+  list.push(1);
+  list.push(2);
+  list.push(3);
+  list.push(4);
+  // This stmt creates LL where 4 is connected to 1
+  list.head.next.next.next.next.next = list.head.next;
+  console.log(list.detectLoop());
+  return list;
+  // 0->1->2->3->4-----|
+  //    |--------<-----
+}
+createCyclicLinkedList();
+
