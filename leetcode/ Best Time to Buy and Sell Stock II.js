bed522fc476b543f5ae57079d4f3502507344610
Yogita Verma <veryogita@gmail.com>
2020-09-19 17:23:47 +0530

    Best Time to Buy and Sell Stock II

--- leetcode/ Best Time to Buy and Sell Stock II.js
+++ leetcode/ Best Time to Buy and Sell Stock II.js
@@ -2,11 +2,11 @@
  * @param {number[]} prices
  * @return {number}
  */
-// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 // Input: [7,1,5,3,6,4]
-// Output: 5
-// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-// Not 7-1 = 6, as selling price needs to be larger than buying price.
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 
 var maxProfit = function (prices) {
   let profit = 0;
